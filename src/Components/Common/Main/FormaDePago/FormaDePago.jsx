02a3e0fe0c459ee975f2851eda29ce3893544a82
Gimenez Santiago <santiago.manuel.gimenez97@gmail.com>
2023-08-01 21:19:26 -0300

    ultimo

--- src/Components/Common/Main/FormaDePago/FormaDePago.jsx
+++ src/Components/Common/Main/FormaDePago/FormaDePago.jsx
@@ -3,11 +3,15 @@ import TarjetaFormaPago from './TarjetaFormaPago/TarjetaFormaPago'
 
 const FormaDePago = () => {
   return (
+    <div className='container-general-formaPago'>
     <div className='container-formaPago'>
     <TarjetaFormaPago />
     <TarjetaFormaPago />
     <TarjetaFormaPago />
+    <div className='displayFormaPago'>
     <TarjetaFormaPago />
+    </div>
+    </div>
     </div>
   )
 }
