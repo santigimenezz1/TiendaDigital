af4f78a683cf39d6b95d34609d88ab004873f74e
Gimenez Santiago <santiago.manuel.gimenez97@gmail.com>
2023-08-05 02:58:44 -0300

    ACORDION

--- src/Components/Common/SectionCategorias/AcordionCategorias/AcordionCategorias.jsx
+++ src/Components/Common/SectionCategorias/AcordionCategorias/AcordionCategorias.jsx
@@ -1,44 +1,104 @@
+import React, { useState } from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import '../AcordionCategorias/acordionCategorias.css'
+import '../AcordionCategorias/acordionCategorias.css';
 import { Link } from 'react-router-dom';
 
 export default function AcordionCategorias() {
+  const [expanded, setExpanded] = useState(true);
+
+  const handleAccordionChange = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <div className='container-acordion'>
-    <Accordion>
-      <AccordionSummary
-      expandIcon={<ExpandMoreIcon sx={{color:"#fd611a"}} />}
-      aria-controls="panel1a-content"
-        id="panel1a-header"
-      >
-        <Typography>Categorias</Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <ul className='lista-accordion'>
-       <Link className="linkCategorias" to={"/categoria/notebook"}><li>Equipos y Notebooks</li></Link> 
-       <Link className="linkCategorias" to={"/categoria/procesadores"}><li>procesadores</li></Link> 
-       <Link className="linkCategorias" to={"/categoria/mothers"}><li>mothers</li></Link> 
-       <Link className="linkCategorias" to={"/categoria/placaVideo"}><li>placas de video</li></Link> 
-       <Link className="linkCategorias" to={"/categoria/memoriaRam"}><li>memorias ram</li></Link> 
-       <Link className="linkCategorias" to={"/categoria/almacenamiento"}><li>almacenamiento</li></Link> 
-       <Link className="linkCategorias" to={"/categoria/regrigeracion"}><li>regrigeracion</li></Link> 
-       <Link className="linkCategorias" to={"/categoria/gabinetes"}><li>gabinetes</li></Link> 
-       <Link className="linkCategorias" to={"/categoria/fuentes"}><li>fuentes</li></Link> 
-       <Link className="linkCategorias" to={"/categoria/monitores"}><li>monitores</li></Link> 
-
-        </ul>
-
-
-            
-       
- 
+      <Accordion expanded={expanded} onChange={handleAccordionChange}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon sx={{ color: "#fd611a" }} />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Categorias</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <ul className='lista-accordion'>
+            <Link
+              className="linkCategorias"
+              to={"/categoria/notebook"}
+              onClick={handleAccordionChange}
+            >
+              <li>Equipos y Notebooks</li>
+            </Link>
+            <Link
+              className="linkCategorias"
+              to={"/categoria/procesadores"}
+              onClick={handleAccordionChange}
+            >
+              <li>procesadores</li>
+            </Link>
+            <Link
+              className="linkCategorias"
+              to={"/categoria/mothers"}
+              onClick={handleAccordionChange}
+            >
+              <li>mothers</li>
+            </Link>
+            <Link
+              className="linkCategorias"
+              to={"/categoria/placaVideo"}
+              onClick={handleAccordionChange}
+            >
+              <li>placas de video</li>
+            </Link>
+            <Link
+              className="linkCategorias"
+              to={"/categoria/memoriaRam"}
+              onClick={handleAccordionChange}
+            >
+              <li>memorias ram</li>
+            </Link>
+            <Link
+              className="linkCategorias"
+              to={"/categoria/almacenamiento"}
+              onClick={handleAccordionChange}
+            >
+              <li>almacenamiento</li>
+            </Link>
+            <Link
+              className="linkCategorias"
+              to={"/categoria/regrigeracion"}
+              onClick={handleAccordionChange}
+            >
+              <li>regrigeracion</li>
+            </Link>
+            <Link
+              className="linkCategorias"
+              to={"/categoria/gabinetes"}
+              onClick={handleAccordionChange}
+            >
+              <li>gabinetes</li>
+            </Link>
+            <Link
+              className="linkCategorias"
+              to={"/categoria/fuentes"}
+              onClick={handleAccordionChange}
+            >
+              <li>fuentes</li>
+            </Link>
+            <Link
+              className="linkCategorias"
+              to={"/categoria/monitores"}
+              onClick={handleAccordionChange}
+            >
+              <li>monitores</li>
+            </Link>
+          </ul>
         </AccordionDetails>
       </Accordion>
-     
     </div>
   );
 }
